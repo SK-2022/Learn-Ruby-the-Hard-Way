e72bbd63bdfac9f7d02c411bbe5cd22875a36dc6
Mika <mikachiusiwa@gmail.com>
2021-11-17 11:58:50 +0200

    Do exercise 6

---
+++ ex6.rb
@@ -0,0 +1,39 @@
+# initializes the variable 'types_of_people' to equal 10
+types_of_people = 10
+
+# initializes a variable in the form of a string with #{} to allow us to input the variable inside the string.
+x = "There are #{types_of_people} types of people. "
+
+# initializes the variable binary to equal the string 'binary'
+binary = "binary"
+
+# initializes the variable do_not to equal "don't"
+do_not = "don't"
+
+# initializes a variable in the form of a string with #{} to allow us to input 2 variables inside the string.
+y = "Those who know #{binary} and those who #{do_not}."
+
+# prints x onto the terminal
+puts x
+
+# prints y onto the terminal
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+# creates a variable and makes it have the boolean value false.
+hilarious = false
+
+#creates a variable joke_evaluation and assigns a string to it.
+joke_evaluation = "Isn't that joke so funny?! #{false}"
+
+# prints joke_evaluation to the terminal.
+puts joke_evaluation 
+
+# creates variable w and e assigning strings to them.
+w = "This is the left side of..."
+e = "a string with a right side."
+
+# prints the concactenated w and e.
+puts w + e
