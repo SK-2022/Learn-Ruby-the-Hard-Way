471bdb34ea1192ff92419398c618899d6d648799
Mika <mikachiusiwa@gmail.com>
2021-11-19 22:01:18 +0200

    Re-do lesson 7 with few additional comments.

--- ex7.rb
+++ ex7.rb
@@ -1,5 +1,5 @@
 puts "Mary had a little lamb."
-puts "It's fleece was white as #{'snow'}"
+puts "Its fleece was white as #{'snow'}."
 puts "And everywhere that Mary went."
 puts "." * 10 # what'd that do?
 
@@ -16,6 +16,6 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# watch that comma at the end. try removing it to see what happens
+#watch that print vs. puts on this line what's it do?
 print end1 + end2 + end3 + end4 + end5 + end6
 puts end7 + end8 + end9 + end10 + end11 + end12
