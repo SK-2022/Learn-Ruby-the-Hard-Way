19a621030971eaca90b49e0d5c1638a9b32123ff
Mika_Chiusiwa <mikachiusiwa@gmail.com>
2022-03-17 23:16:40 +0900

    add arrival in japan date

--- ex1.rb
+++ ex1.rb
@@ -6,3 +6,5 @@
 #puts "I'd much rather you 'not'."
 #puts 'I "said" do not touch this.'
 puts "I'm going to Japan in March 2022"
+
+puts "Update. I've arrive in Japan as of March 13 2022. Thank you GOD."
